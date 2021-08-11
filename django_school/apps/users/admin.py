9621bd00e6dfcc3d58f71823f7ce264da74f9586
Radoslaw <r.cybulski1122012@gmail.com>
2021-08-11 17:26:12 +0200

    Add fields to user creation form in admin view

--- django_school/apps/users/admin.py
+++ django_school/apps/users/admin.py
@@ -5,5 +5,36 @@ from django_school.apps.users.models import User
 
 
 @admin.register(User)
-class UserAdmin(UserAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            None,
+            {
+                "fields": (
+                    "personal_id",
+                    "phone_number",
+                    "gender",
+                    "address",
+                    "school_class",
+                )
+            },
+        ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            None,
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "personal_id",
+                    "phone_number",
+                    "gender",
+                    "address",
+                    "school_class",
+                )
+            },
+        ),
+    )
