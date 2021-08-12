cd05b65ad5c4fce8e5729d9140b45a3d9c7a763e
Radoslaw <r.cybulski1122012@gmail.com>
2021-08-12 18:52:35 +0200

    Extend generate_exact_lessons command
    Now it also generates presences objects for students.

--- django_school/apps/lessons/management/commands/generate_exact_lessons.py
+++ django_school/apps/lessons/management/commands/generate_exact_lessons.py
@@ -2,15 +2,26 @@ from datetime import date
 
 from django.core.management import BaseCommand
 
-from django_school.apps.lessons.models import ExactLesson, Lesson
+from django_school.apps.lessons.models import ExactLesson, Lesson, Presence
 
 
 class Command(BaseCommand):
-    help = "Creates ExactLessons objects for lessons planned for today."
+    help = "Creates ExactLessons ans Presences objects for lessons planned for today."
 
     def handle(self, *args, **kwargs):
         current_day = date.today().strftime("%a").lower()
-        lessons = Lesson.objects.filter(weekday=current_day)
+        lessons = (
+            Lesson.objects.filter(weekday=current_day)
+            .select_related("school_class")
+            .prefetch_related("school_class__students")
+        )
 
         for lesson in lessons:
-            ExactLesson.objects.create(lesson=lesson)
+            exact_lesson = ExactLesson.objects.create(lesson=lesson)
+
+            presences = [
+                Presence(student=student, exact_lesson=exact_lesson, status="none")
+                for student in lesson.school_class.students.all()
+            ]
+
+            Presence.objects.bulk_create(presences)
--- tests/lessons/test_commands.py
+++ tests/lessons/test_commands.py
@@ -3,7 +3,7 @@ import datetime
 from django.core.management import call_command
 from django.test import TestCase
 
-from django_school.apps.lessons.models import ExactLesson
+from django_school.apps.lessons.models import ExactLesson, Presence
 from tests.utils import ClassesMixin, LessonsMixin, UsersMixin
 
 
@@ -17,17 +17,39 @@ datetime.date = DateMock
 
 
 class TestGenerateExactLessons(LessonsMixin, UsersMixin, ClassesMixin, TestCase):
-    def test_creates_exact_lessons_for_today(self):
-        subject = self.create_subject()
-        teacher = self.create_teacher()
-        school_class = self.create_class()
-        friday_lesson = self.create_lesson(
-            subject, teacher, school_class, weekday="fri"
+    def setUp(self):
+        self.subject = self.create_subject()
+        self.teacher = self.create_teacher()
+        self.school_class = self.create_class()
+        self.student = self.create_user(
+            username="Student123", school_class=self.school_class
         )
-        self.create_lesson(subject, teacher, school_class, weekday="mon")
+
+    def test_creates_exact_lessons_for_today_and_presences(self):
+        friday_lesson = self.create_lesson(
+            self.subject, self.teacher, self.school_class, weekday="fri"
+        )
+        self.create_lesson(self.subject, self.teacher, self.school_class, weekday="mon")
 
         call_command("generate_exact_lessons")
 
-        exact_lessons = ExactLesson.objects.all()
-        self.assertEqual(len(exact_lessons), 1)
-        self.assertEqual(exact_lessons[0].lesson, friday_lesson)
+        exact_lesson = ExactLesson.objects.all()[0]
+        presence = Presence.objects.get(exact_lesson=exact_lesson)
+        self.assertEqual(exact_lesson.lesson, friday_lesson)
+        self.assertEqual(presence.student, self.student)
+        self.assertEqual(presence.exact_lesson, exact_lesson)
+        self.assertEqual(presence.status, "none")
+
+    def test_performs_optimal_number_of_queries(self):
+        student2 = self.create_user(
+            username="Student321", school_class=self.school_class
+        )
+        lessons = [
+            self.create_lesson(
+                self.subject, self.teacher, self.school_class, weekday="fri"
+            )
+            for _ in range(5)
+        ]
+
+        with self.assertNumQueries(12):
+            call_command("generate_exact_lessons")
